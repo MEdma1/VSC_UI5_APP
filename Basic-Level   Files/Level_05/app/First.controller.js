33d94c5188dc1992c5280210c2f4a3cd47df2c7a
ME <marco.erhard@dma-solutions.de>
2024-03-14 16:53:24 +0100

    c

--- Basic-Level   Files/Level_05/app/First.controller.js
+++ Basic-Level   Files/Level_05/app/First.controller.js
@@ -1,12 +1,24 @@
 
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
 	"sap/m/MessageToast"
-	], function (Controller, MessageToast) { 
+	], function (Controller, JSONModel, MessageToast) { 
 	'use strict';
+
 	return Controller.extend("app.First",{
+
+
+		onInit: function () {
+			const oModel = new JSONModel();
+			this.getView().setModel("./data2.json");
+		},
+
+
 		handlePress : function(){
 			MessageToast.show("VSC-Output done!");
 		}
 	});
+
+
 });
