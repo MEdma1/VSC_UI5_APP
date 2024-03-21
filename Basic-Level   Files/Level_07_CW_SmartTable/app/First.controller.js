3bfd9bedd26c5fb88e0a964330891ecf94eef81c
ME <marco.erhard@dma-solutions.de>
2024-03-21 15:04:59 +0100

    c

--- Basic-Level   Files/Level_07_CW_SmartTable/app/First.controller.js
+++ Basic-Level   Files/Level_07_CW_SmartTable/app/First.controller.js
@@ -27,13 +27,43 @@ sap.ui.define([
             fetch(url)
             .then((res)=>res.json())
             .then((param)=>{
+                console.log(new JSONModel(param));
                 this.getView().setModel(new JSONModel(param));
             });
-        }
+        },
 
 
+        onEdit: function() {
+			// this.aProductCollection = deepExtend([], this.oModel.getProperty("/ProductCollection"));
+			this.byId("editButton").setVisible(false);
+			this.byId("saveButton").setVisible(true);
+			this.byId("cancelButton").setVisible(true);
+			// this.rebindTable(this.oEditableTemplate, "Edit");
+		},
 
+		onSave: function() {
+			this.byId("saveButton").setVisible(false);
+			this.byId("cancelButton").setVisible(false);
+			this.byId("editButton").setVisible(true);
+			// this.rebindTable(this.oReadOnlyTemplate, "Navigation");
+		},
 
+		onCancel: function() {
+			this.byId("cancelButton").setVisible(false);
+			this.byId("saveButton").setVisible(false);
+			this.byId("editButton").setVisible(true);
+			// this.oModel.setProperty("/ProductCollection", this.aProductCollection);
+			// this.rebindTable(this.oReadOnlyTemplate, "Navigation");
+		},
+
+		rebindTable: function(oTemplate, sKeyboardMode) {
+			// this.oTable.bindItems({
+			// 	path: "/ProductCollection",
+			// 	template: oTemplate,
+			// 	templateShareable: true,
+			// 	key: "ProductId"
+			// });
+		},
 
 	});
 
