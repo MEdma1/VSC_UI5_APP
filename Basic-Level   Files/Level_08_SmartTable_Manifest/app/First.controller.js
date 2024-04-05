6e23d80b31eda4592c85fb4912db7cdb48dccbeb
ME <marco.erhard@dma-solutions.de>
2024-04-05 18:19:49 +0200

    c

--- Basic-Level   Files/Level_08_SmartTable_Manifest/app/First.controller.js
+++ Basic-Level   Files/Level_08_SmartTable_Manifest/app/First.controller.js
@@ -1,84 +1,50 @@
 
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/m/MessageToast"
-	], function (Controller, JSONModel, MessageToast) { 
+	"sap/ui/model/odata/v4/ODataModel"
+	], function (Controller, ODataModel) { 
 	'use strict';
 
         var oModel = new JSONModel(); 
 
 	return Controller.extend("app.First",{
 
-        
-
 		onInit: function () {
+
+			var oModel = new ODataModel({
+				serviceUrl : "https://services.odata.org/TripPinRESTierService/(S(po15j3rhuv503vpmxcq2bkd3))/"
+			});
+
+			console.log(oModel);
+
+			
+			this.getView().setModel(oModel);
 
 		},
 
 		
-        onAfterRendering : function(){
-            // this.onGetRequest('https://services.odata.org/V4/(S(epugafwj5m0rg30yoxbtltlk))/TripPinServiceRW/?$format=json');
-			// this.onGetRequest('https://services.odata.org/V3/Northwind/Northwind.svc/Products?$format=json');
-			this.onGetRequest('https://services.odata.org/TripPinRESTierService/(S(zdjpqz5q4hlpngaq3zdkdzzv))/Airports?$format=json');
-        },
-
-
-
-        onGetRequest : function(url){
-            fetch(url)
-            .then((res)=>res.json())
-            .then((param)=>{
-                this.getView().setModel(new JSONModel(param));
-
-                oModel(param);
-                console.log(oModel);
-
-            });
-        },
-
-
-
-
-
-
-
 
 
 
 
 
         onEdit: function() {
-			// this.aProductCollection = deepExtend([], this.oModel.getProperty("/value"));
 			this.byId("editButton").setVisible(false);
 			this.byId("saveButton").setVisible(true);
 			this.byId("cancelButton").setVisible(true);
-			// this.rebindTable(this.oEditableTemplate, "Edit");
 		},
 
 		onSave: function() {
 			this.byId("saveButton").setVisible(false);
 			this.byId("cancelButton").setVisible(false);
 			this.byId("editButton").setVisible(true);
-			// this.rebindTable(this.oReadOnlyTemplate, "Navigation");
 		},
 
 		onCancel: function() {
 			this.byId("cancelButton").setVisible(false);
 			this.byId("saveButton").setVisible(false);
 			this.byId("editButton").setVisible(true);
-			// this.oModel.setProperty("/ProductCollection", this.aProductCollection);
-			// this.rebindTable(this.oReadOnlyTemplate, "Navigation");
 		},
-
-		// rebindTable: function(oTemplate, sKeyboardMode) {
-			// this.oTable.bindItems({
-			// 	path: "/ProductCollection",
-			// 	template: oTemplate,
-			// 	templateShareable: true,
-			// 	key: "ProductID"
-			// });
-		// },
 
 	});
 
